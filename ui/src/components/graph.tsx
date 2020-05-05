bddc69f5ecc7b648e4966193cf7d3abce6615e2c
Yaroslav <y.dashkevich@pm.me>
2020-05-05 12:02:45 +0300

    Add color

--- ui/src/components/graph.tsx
+++ ui/src/components/graph.tsx
@@ -5,9 +5,40 @@ import { ISearchResultDto } from "../types/dto";
 import { constants } from '../constants';
 
 
+const backgroundColors = [
+  'rgba(213, 120, 91, 0.2)',
+  'rgba(195, 131, 68, 0.2)',
+  'rgba(167, 144, 59, 0.2)',
+  'rgba(134, 154, 70, 0.2)',
+  'rgba(98, 160, 96, 0.2)',
+  'rgba(64, 162, 128, 0.2)',
+  'rgba(50, 161, 159, 0.2)',
+  'rgba(81, 156, 180, 0.2)',
+  'rgba(129, 146, 188, 0.2)',
+  'rgba(172, 133, 179, 0.2)',
+  'rgba(204, 122, 156, 0.2)',
+  'rgba(219, 116, 125, 0.2)',
+];
+const borderColors = [
+  'rgba(213, 120, 91, 1)',
+  'rgba(195, 131, 68, 1)',
+  'rgba(167, 144, 59, 1)',
+  'rgba(134, 154, 70, 1)',
+  'rgba(98, 160, 96, 1)',
+  'rgba(64, 162, 128, 1)',
+  'rgba(50, 161, 159, 1)',
+  'rgba(81, 156, 180, 1)',
+  'rgba(129, 146, 188, 1)',
+  'rgba(172, 133, 179, 1)',
+  'rgba(204, 122, 156, 1)',
+  'rgba(219, 116, 125, 1)',
+];
+
 interface IDatasetItem {
   label: string;
   data: number[];
+  backgroundColor?: string;
+  borderColor?: string;
 }
 
 let _labels: string[] = [];
@@ -47,10 +78,12 @@ function createDataset(data: ISearchResultDto | null) {
       _labels.push(`${dateStr} (${dayOfWeek})`);
     }
 
-    arr.forEach(item => {
+    arr.forEach((item, index) => {
       const _d: IDatasetItem = {
         label: item.name,
         data: [],
+        backgroundColor: backgroundColors[index % backgroundColors.length],
+        borderColor: 'rgba(0, 0, 0, 0)',
       };
       for (
         let date = new Date(from).getTime();
@@ -71,7 +104,6 @@ function createDataset(data: ISearchResultDto | null) {
 export default function Graph({ data }: { data: ISearchResultDto | null }) {
   const [labels, datasets] = createDataset(data);
 
-  console.log(datasets)
   return <section className="section">
     <div className='container'>
 
